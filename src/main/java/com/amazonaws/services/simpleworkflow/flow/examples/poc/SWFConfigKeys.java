9b8a6fae587f00616c9700eadb3489aeb263a1c1
Sanket Maru <sanketmaru@users.noreply.github.com>
2015-09-30 08:55:32 +0530

    remove bucket names

--- src/main/java/com/amazonaws/services/simpleworkflow/flow/examples/poc/SWFConfigKeys.java
+++ src/main/java/com/amazonaws/services/simpleworkflow/flow/examples/poc/SWFConfigKeys.java
@@ -15,16 +15,15 @@
 package com.amazonaws.services.simpleworkflow.flow.examples.poc;
 
 class SWFConfigKeys {
-	 public static final String ACTIVITY_WORKER_LOCALFOLDER = "/home/webonise/sanket_test";
+	 public static final String ACTIVITY_WORKER_LOCALFOLDER = "/home/sanket_test";
 	    public static final String ACTIVITY_WORKER_COMMON_TASKLIST = "mytasklist";
 	    public static final String WORKFLOW_WORKER_TASKLIST = "poctasklist";
 	    
 	    
 	    public static final String WORKFLOW_EXECUTION_ID_KEY= "1";
-	    public static final String WORKFLOW_INPUT_SOURCEFILENAME_KEY =
-	            "50/2F732D2D-E129-4763-ETREYRTY-TTTTTTTTTT/sources/sensordata/DSC_2078.jpg";
-	    public static final String WORKFLOW_INPUT_SOURCEBUCKETNAME_KEY = "precisionmapper-test";
-	    public static final String WORKFLOW_INPUT_BUCKET_KEY = "precisionmapper-test";
+	    public static final String WORKFLOW_INPUT_SOURCEFILENAME_KEY = ""; // input path for bucket
+	    public static final String WORKFLOW_INPUT_SOURCEBUCKETNAME_KEY = ""; // bucket name
+	    public static final String WORKFLOW_INPUT_BUCKET_KEY = ""; // bucket name
 	    public static final String WORKFLOW_INPUT_IMAGEPROCESSINGOPTION_KEY = "GRAY_SCALE";
 
 }
